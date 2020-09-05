6c496fb46d86e1dbea794fdefb8fda8676d4e455
Fernando <fernando.kanashiro@valemobi.com.br>
2020-09-05 17:40:55 -0300

    Iniciando o portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { ContactComponent } from './routes/contact/contact.component';
+import { SkillsComponent } from './routes/skills/skills.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  },
+  {
+    path: 'home', component: HomeComponent
+  },
+  {
+    path: 'contact', component: ContactComponent
+  },
+  {
+    path: 'skills', component: SkillsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './routes/home/home.component';
+import { HomeHeaderComponent } from './shared/home-header/home-header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { SkillsComponent } from './routes/skills/skills.component';
+import { ContactComponent } from './routes/contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+      AppComponent,
+      HomeComponent,
+      HomeHeaderComponent,
+      SkillsComponent,
+      ContactComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    BsDropdownModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [BsDropdownModule]
 })
 export class AppModule { }
